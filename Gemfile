003ac4f5b311707eeb655f3827f4b4dd513372b7
Yi Wen <ywen.github@gmail.com>
2013-06-07 09:46:01 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in gh_repo_stats.gemspec
+gemspec
---
+++ lib/gh_repo_stats.rb
@@ -0,0 +1,7 @@
+Dir["#{File.dirname(__FILE__)}/gh_repo_stats/**/*.rb"].each do |f| 
+  require f
+end
+
+module GhRepoStats
+  # Your code goes here...
+end
